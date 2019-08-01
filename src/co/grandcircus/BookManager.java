c3e6d44c307fdcf2755d23da8209e4856a51ff4c
Kris Cherven <krischerven@gmail.com>
2019-08-01 14:08:19 -0400

    adds the BookManager (halfway done?)

--- src/co/grandcircus/BookManager.java
+++ src/co/grandcircus/BookManager.java
@@ -1,3 +1,19 @@
-public final class BookManager {
+package co.grandcircus
+import java.util.Vector;
 
+public final class BookManager {
+	// immutable list of books
+	private final Vector<Book> books = new Vector<>();
+	// function definitions
+	public String getDisplayString() {
+		String ret = "\n";
+		for (Book book : books) {
+			ret += book.toString(); 
+			ret += "\n";
+		}
+		return ret;
+	}
+	public void addBook(Book book) {
+		books.add(book);
+	}
 }
