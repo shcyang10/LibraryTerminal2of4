11520e130621e24a2c8587fc96ab6c88d47d2e74
Brian-Hudson <allerka@gmail.com>
2019-08-01 15:32:31 -0400

    further Library cleanup

--- src/co/grandcircus/Library.java
+++ src/co/grandcircus/Library.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public final class Library {
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int userInput;
@@ -24,68 +24,68 @@ public final class Library {
 		// input to ask if they want to check book out
 		// input to ask if they want to return the book
 		while (cont.equalsIgnoreCase("y")) {
-		menu();
-		
-		userInput = scan.nextInt();
-		userSelection(scan, userInput);
-		
-		cont = Validator.getStringMatchingRegex(scan, "Would you like to continue?(y/n) ", "[YyNn]");
+			System.out.println(
+					"1. Display list\n2. Search by author\n3. Search by title\n4. Check book out\n5. Return book\n6. Add book");
+			userInput = Validator.getInt(scan, "Select an option: ", 1, 6);
+			userSelection(scan, userInput);
+			cont = Validator.getStringMatchingRegex(scan, "Would you like to continue?(y/n) ", "[YyNn]");
 		}
 	}
-	public static void menu() {
-		System.out.println("1. Display list\n2. Search by author\n3. Search by title\n4. Check book out\n5. Return book\n6. Add book");
-	}
-	public static String userSelection(Scanner scanner, int input) {
-		switch(input) {
+
+	public static String userSelection(Scanner scan, int userInput) {
+		BookManager b = new BookManager();
+		switch (userInput) {
 		case 1:
-		//return readFromFile();
-		
+			break;
+		// return readFromFile();
+
 		case 2:
 			String str;
+			scan.nextLine();
 			System.out.println("Type in an author");
-			str = scanner.nextLine();
-		//return	BookManager.getDisplayString(str);
-			
+			str = scan.nextLine();
+			return b.getDisplayString(str);
 		case 3:
-		//return	searchTitle();
-		
+			// return searchTitle();
+			break;
 		case 4:
-		//return	checkBookOut();
-			
+			// return checkBookOut();
+			break;
 		case 5:
-		//return	returnBook();
+			// return returnBook();
 		case 6:
+			scan.nextLine();
 			String str3;
 			String str2;
 			System.out.println("Enter the title of the book: ");
-			str3 = scanner.nextLine();
+			str3 = scan.nextLine();
 			System.out.println("Enter name of author: ");
-			str2 = scanner.nextLine();
-			//BookManager.addBook(str, str2);
+			str2 = scan.nextLine();
+			b.addBook(new Book(str3, str2, "on Shelf", null, null));
 		}
 		return " "; // remove this later it's not needed
 	}
-	
+
 	public static void writeToFile(ArrayList<Book> bookList) {
-        String fileName = "booklist.txt";
-        Path path = Paths.get("library", fileName);
-        File file = path.toFile();
-        PrintWriter output = null;
-        try {
-            output = new PrintWriter(new FileOutputStream(file, true));
-            output.println();
-            /*
-             *
-             *
-             * add the name of array of books
-             *
-             *
-             */
-        } catch (FileNotFoundException e) {
-            // e.printStackTrace();
-            System.out.println("Hey, contact customer service");
-        } finally {
-            output.close();
-        }
-    }
-}	
+		String fileName = "booklist.txt";
+		Path path = Paths.get("library", fileName);
+		File file = path.toFile();
+		PrintWriter output = null;
+		try {
+			output = new PrintWriter(new FileOutputStream(file, true));
+			output.println();
+			/*
+			 *
+			 *
+			 * add the name of array of books
+			 *
+			 *
+			 */
+		} catch (FileNotFoundException e) {
+			// e.printStackTrace();
+			System.out.println("Hey, contact customer service");
+		} finally {
+			output.close();
+		}
+	}
+}
