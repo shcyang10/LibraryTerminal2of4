3f409ebb32646871a2204d905ab2fb80e8c2f3c1
Kris Cherven <krischerven@gmail.com>
2019-08-01 13:36:52 -0400

    Adds the Book class (WIP)

--- src/co/grandcircus/Book.java
+++ src/co/grandcircus/Book.java
@@ -1,6 +1,14 @@
 package co.grandcircus;
 
 public final class Book {
-	
-
+	private String title;
+	private String author;
+	private String dueAt;
+	private Status status;
+	public Book(String _title, String _author, String _dueAt, Status _status) {
+		title = _title;
+		author = _author;
+		dueAt = _dueAt;
+		status = _status;
+	}
 }
