9ab9c14217418da0651c1c8d1cedfcc07363acc7
Kris Cherven <krischerven@gmail.com>
2019-08-02 11:08:55 -0400

    tiny formatting changing

--- src/co/grandcircus/Category.java
+++ src/co/grandcircus/Category.java
@@ -1,8 +1,7 @@
 package co.grandcircus;
 
 public enum Category {
-FICTION, NONFICTION, SCIENCE_FICTION, FANTASY, PHILOSOPHY, DRAMA, SCIENCE;
-	
+	FICTION, NONFICTION, SCIENCE_FICTION, FANTASY, PHILOSOPHY, DRAMA, SCIENCE;
 	@Override
 	public String toString() {
 		return this.name().toLowerCase().replace("_", " ");
--- src/co/grandcircus/Status.java
+++ src/co/grandcircus/Status.java
@@ -2,7 +2,6 @@ package co.grandcircus;
 
 public enum Status {
 	ON_SHELF, CHECKED_OUT;
-
 	@Override
 	public String toString() {
 		return this.name().toLowerCase().replace("_", " ");
