4892dcef9af44ba597018552f0d1015b1e15a256
Kbusch54 <kevinbusch@comcast.net>
2019-08-02 11:28:18 -0400

    Cleaned up library

--- src/co/grandcircus/Library.java
+++ src/co/grandcircus/Library.java
@@ -6,28 +6,19 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public final class Library {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		ArrayList<Book> bookList = new ArrayList<>();
+
 		int userInput;
 		String cont = "y";
 		BookManager b = new BookManager();
-		//readFromFile(bookList);
 
 		System.out.println("Welcome to the Library of Alexandria");
-		// display list
-		// need textfile for methods
-		// numbers to select from the list
-		// input to search by author
-		// input to search by title
-		// input to ask if they want to check book out
-		// input to ask if they want to return the book
+
 		while (cont.equalsIgnoreCase("y")) {
 			System.out.println(
 					"1. Display list\n2. Search by author\n3. Search by title\n4. Check book out\n5. Return book\n6. Add book");
@@ -41,10 +32,7 @@ public final class Library {
 	public static void userSelection(Scanner scan, int userInput, BookManager b) {
 		switch (userInput) {
 		case 1:
-		
-//			for (Book output : bookList) {
-//				System.out.println(output);	
-//				}
+
 			System.out.println(b.getDisplayString(null, null));
 			break;
 		// return readFromFile();
@@ -71,7 +59,7 @@ public final class Library {
 			int i = scan.nextInt();
 			b.getBookByNumber(i).setStatus(co);
 			break;
-			// checkout book
+		// checkout book
 		case 5:
 			System.out.println(b.getDisplayString(null, null));
 			Status os = Status.ON_SHELF;
@@ -79,7 +67,7 @@ public final class Library {
 			int x = scan.nextInt();
 			b.getBookByNumber(x).setStatus(os);
 			break;
-			// return Book
+		// return Book
 		case 6:
 			String str3;
 			String str2;
@@ -88,32 +76,9 @@ public final class Library {
 			System.out.println("Enter name of author: ");
 			str2 = scan.nextLine();
 			b.addBook(new Book(str3, str2, "NULL", Status.ON_SHELF, Category.FICTION));
-		// add category choice
+			// add category choice
 		}
 	}
-
-//	public static void readFromFile(ArrayList<Book> bookList) {
-//		String fileName = "booklist.txt";
-//		Path path = Paths.get(fileName);
-//		File file = path.toFile();
-//		try {
-//			BufferedReader br = new BufferedReader(new FileReader(file));
-//			String line = br.readLine();
-//
-//			while (line != null) {
-//				String[] bookReader = line.split(",");
-//				Book b = new Book(bookReader[0], bookReader[1], bookReader[2], 
-//						Status.valueOf(bookReader[3]), Category.valueOf(bookReader[4]));
-//				bookList.add(b);
-//				line = br.readLine();
-//			}
-//			br.close();
-//		} catch (FileNotFoundException e) {
-//			System.out.println("File not found.");
-//		} catch (IOException e) {
-//			System.out.println("File read error.");
-//		}
-//	}
 
 	public static void writeToFile(BookManager b) {
 		String fileName = "booklist.txt";
@@ -123,23 +88,17 @@ public final class Library {
 		try {
 			output = new PrintWriter(new FileOutputStream(file, false));
 			for (Book b1 : b.getBooks()) {
-			String b2 = b1.toString().replaceAll(" {2,}", "").replaceAll("(by)|(\\(Due at)|(\\))", ",")
-					.replaceAll("(on shelf)", "ON_SHELF,").replaceAll("(checked out)", "CHECKED_OUT,")
-					.replaceAll("(nonfiction)", "NONFICTION").replaceAll("(science fiction)", "SCIENCE_FICTION")
-					.replaceAll("(fiction)", "FICTION").replaceAll("(fantasy)", "FANTASY")
-					.replaceAll("(science)", "SCIENCE").replaceAll("(philosophy)", "PHILOSOPHY")
-					.replaceAll("(drama)", "DRAMA");
+				String b2 = b1.toString().replaceAll(" {2,}", "").replaceAll("(by)|(\\(Due at)|(\\))", ",")
+						.replaceAll("(on shelf)", "ON_SHELF,").replaceAll("(checked out)", "CHECKED_OUT,")
+						.replaceAll("(nonfiction)", "NONFICTION").replaceAll("(science fiction)", "SCIENCE_FICTION")
+						.replaceAll("(fiction)", "FICTION").replaceAll("(fantasy)", "FANTASY")
+						.replaceAll("(science)", "SCIENCE").replaceAll("(philosophy)", "PHILOSOPHY")
+						.replaceAll("(drama)", "DRAMA");
 				output.println(b2);
 			}
-			/*
-			 *
-			 *
-			 * add the name of array of books
-			 *
-			 *
-			 */
+
 		} catch (FileNotFoundException e) {
-			// e.printStackTrace();
+		
 			System.out.println("File not found.");
 		} finally {
 			output.close();
