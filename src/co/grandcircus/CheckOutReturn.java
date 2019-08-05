d0ec4c57f960111b5523b0652b26dbb7dbc02741
Kris Cherven <krischerven@gmail.com>
2019-08-05 13:12:39 -0400

    condenses CheckOutReturn.java and removes an unused variable

--- src/co/grandcircus/CheckOutReturn.java
+++ src/co/grandcircus/CheckOutReturn.java
@@ -1,16 +1,11 @@
 package co.grandcircus;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
 
 public class CheckOutReturn {
-	final static Scanner scan = new Scanner(System.in);
 	public static String dueDate(String dueAt) {
-		LocalDate ld;
-		ld = LocalDate.now().plusDays(14);
+		LocalDate ld = LocalDate.now().plusDays(14);
 		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-		dueAt = ld.format(dateFormat);
-		return dueAt;
+		return ld.format(dateFormat);
 	}
-
 }
