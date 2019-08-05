7edea23bf5676df213788fe8835fb567d78cf0cd
Kris Cherven <krischerven@gmail.com>
2019-08-05 11:15:07 -0400

    removes a useless library

--- src/co/grandcircus/CheckOutReturn.java
+++ src/co/grandcircus/CheckOutReturn.java
@@ -1,13 +1,10 @@
 package co.grandcircus;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class CheckOutReturn {
 	static Scanner scan = new Scanner(System.in);
-	String dueAt;
-
 	public static String dueDate(String dueAt) {
 		LocalDate ld;
 		ld = LocalDate.now().plusDays(14);
