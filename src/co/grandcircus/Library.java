c37407ddf34545a789016ba7aa3d01014e8fea1b
Kbusch54 <kevinbusch@comcast.net>
2019-08-05 14:28:29 -0400

    The

--- src/co/grandcircus/Library.java
+++ src/co/grandcircus/Library.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 public class Library {
 
-	final static String GOODBYE_MSG = "\nGoodbye! Thank you for coming to the Library of Alexandria.\n";
+	final static String GOODBYE_MSG = "\nGoodbye! Thank you for coming to The Library of Alexandria.\n";
 
 	public static void main(String[] args) {
 
